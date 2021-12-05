2bebf0e3a7fa622730c5da244db09aff22db6963
rajonna <rajanyabose145@gmail.com>
2021-12-05 19:41:06 +0530

    done

---
+++ numberguesser.js
@@ -0,0 +1,21 @@
+var num = Math.floor((Math.random() * 100) + 1);
+console.log(num)
+var c1 = 0
+function myFunction(){
+  c1 = c1+1
+  document.getElementById("count").innerHTML =`Guess count ${c1} `
+  var v = document.getElementById("inp").value;
+  if( v == num){
+    document.getElementById("guess").innerHTML = "CORRECT ANSWER"
+    document.getElementById("guessdisplay").innerHTML = `Took you ${c1} guesses`
+    document.getElementById("count").style.display="none";
+  }
+  else{
+    if (v>num) {
+      document.getElementById("guess").innerHTML = "you guessed: too high"
+    }
+    else{
+      document.getElementById("guess").innerHTML = "you guessed: too low"
+    }
+  }
+}
